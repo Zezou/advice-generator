4e723a3392ccad08622b797e864cfa5a18ea4d56
Zezou <Zezou@users.noreply.github.com>
2025-03-19 14:47:32 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const adviceText = document.getElementById("advice-text");
+const adviceId = document.getElementById("advice-id");
+const loading = document.getElementById("loading");
+const advice = document.getElementById("advice");
+
+const displayLoading = async () => {
+  advice.classList.toggle("hidden");
+  loading.classList.toggle("hidden");
+};
+
+const displayData = async (slip) => {
+  adviceId.textContent = slip.id;
+  adviceText.textContent = slip.advice;
+  loading.classList.toggle("hidden");
+  advice.classList.toggle("hidden");
+};
+
+const getData = async () => {
+  const url = "https://api.adviceslip.com/advices";
+  displayLoading();
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(
+        `Sometimes the internet returns error code ${response.status}`
+      );
+    }
+    const data = await response.json();
+    displayData(data.slip);
+  } catch (error) {
+    const slip = { id: 0, advice: error.message };
+    displayData(slip);
+    console.error(error.message);
+  }
+};
+
+getData();
